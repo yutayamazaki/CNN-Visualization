203e0d6dadd6819df6f7eb7d32b90c13a798b5a3
yutayamazaki <tppymd@gmail.com>
2019-05-30 20:28:45 +0900

    clean codes

--- visualizations/config.py
+++ visualizations/config.py
@@ -2,12 +2,3 @@ IMG_DIR = '../img/'
 SAMPLE_IMG = 'cat.jpg'
 
 IMG_SIZE = 224
-
-from torchvision import transforms
-
-transform = transforms.Compose
-
-transform = transforms.Compose([
-    transforms.Resize((IMG_SIZE, IMG_SIZE)),
-    transforms.ToTensor()
-])
